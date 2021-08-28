4e94d477ab6a49989f8324d65b2fa6ffeaea83d4
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-08-28 17:44:54 -0300

    Developed controller with Get, GetList, Post, Put and Delete Mappings

--- src/main/java/com/dio/saladereuniao/exception/ResourceNotFoundException.java
+++ src/main/java/com/dio/saladereuniao/exception/ResourceNotFoundException.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception{
     private static final long serialVersionUID=1L;
-    public resourceNotFoundException(String message){
+
+    public ResourceNotFoundException(String message){
         super(message);
     }
 }
